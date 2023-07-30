6af688de1719fe95e756eecb7e03726b1d3c1c5f
tim <timdedenker@gmail.com>
2023-07-30 18:54:18 +0200

    fixed GetPositionScore and compressed it to the limit, just like with the piecePositionValueTable array. also added another c# project in the repo called IntToHex, which is a script that outputs my tightly packed piece position value array in 64-bit hex

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -17,77 +17,17 @@ public class MyBot : IChessBot
     Dictionary<ulong, int> transpositionTable = new();
     int[] pieceValues = {100, 320, 330, 500, 900, 20000};
 
-    // ulong[,] piecePositionValueTable =
-    // {
-    // {0x0000000000000000, 0x050A0AECEC0A0A05, 0x05FBF600F6FB0505, 0x0000001414000000, 0x05050A19190A0505, 0x0A0A141E1E140A0A, 0x3232323232323232, 0x0000000000000000},
-    // {0xCEC8E2E2E2E2C8CE, 0xC8D800050500D8C8, 0xE2050A0F0F0A05E2, 0xE2000F1414140FE2, 0xE2051514141505E2, 0xE20005100F1005E2, 0xC8D80000FF00D8C8, 0xCEC8E2E2E2E2C8CE},
-    // {0xECEAEAEAEAEAEAEA, 0xEAF500000000F5EA, 0xEAF5F5F5F5F5F5EA, 0xEA00F5F5F5F500EA, 0xF50505F5F50505F5, 0xF50005F5F50500F5, 0xF50000FFFFFF00F5, 0xECEAEAEAEAEAEAEA},
-    // {0x0000000505000000, 0xFB000000000000FB, 0xFB000000000000FB, 0xFB000000000000FB, 0xFB000000000000FB, 0xFB000000000000FB, 0x050AFAFAFAFAFA05, 0x0000050505000000},
-    // {0xECEAEAA5A5AAEAEA, 0xEAF500050505F5EA, 0xF50005050505F5FB, 0xFB00555555FFFBFB, 0xFBFF55555500FBFB, 0xFA005555550005FA, 0xFA00FFFFFFFF00FA, 0xECEAA5A5A5A5EAEA},
-    // {0x141E0A00000A1E14, 0x1414000000001414, 0xF6E6E6E6E6E6E6F6, 0xECE2E2D8D8E2E2EC, 0xD8D8D8C8C8D8D8D8, 0xD8D8D8C8C8D8D8D8, 0xD8D8D8C8C8D8D8D8, 0x1414000014141414}
-    // };
-    int GetPositionScore(int pieceType, int index) => (sbyte)((piecePositionValueTable[pieceType, index / 8] >> (8 * (index % 8))) & 0xFF);
+    ulong[] piecePositionValueTable = {
+        0x00000000050A0AEC, 0x05FBF60000000014, 0x05050A190A0A141E, 0x3232323200000000, // pawns
+        0xCED8E2E2D8EC0005, 0xE2050A0FE2000F14, 0xE2050F14E2000A0F, 0xD8EC0000CED8E2E2, // knights
+        0xECF6F6F6F6050000, 0xF60A0A0AF6000A0A, 0xF605050AF600050A, 0xF6000000ECF6F6F6, // bishops
+        0x00000005FB000000, 0xFB000000FB000000, 0xFB000000FB000000, 0x050A0A0A00000000, // rooks
+        0xECF6F6FBF6000000, 0xF605050500000505, 0xFB000505F6000505, 0xF6000000ECF6F6FB, // queens
+        0x141E0A0014140000, 0xF6ECECECECE2E2D8, 0xE2D8D8CEE2D8D8CE, 0xE2D8D8CEE2D8D8CE  // kings
+    };
 
-    // values for WHITE player. for BLACK player, index needs to be mirrored or matrix 180 rotated
-    static int[] pawnTable = { 0, 0, 0, 0, 0, 0, 0, 0,
-                        5, 10, 10, -20, -20, 10, 10, 5,
-                        5, -5, -10, 0, 0, -10, -5, 5,
-                        0, 0, 0, 20, 20, 0, 0, 0,
-                        5, 5, 10, 25, 25, 10, 5, 5,
-                        10, 10, 20, 30, 30, 20, 10, 10,
-                        50, 50, 50, 50, 50, 50, 50, 50,
-                        0, 0, 0, 0, 0, 0, 0, 0 };
-    static int[] knightTable = {
-    -50, -40, -30, -30, -30, -30, -40, -50,
-    -40, -20, 0, 5, 5, 0, -20, -40,
-    -30, 5, 10, 15, 15, 10, 5, -30,
-    -30, 0, 15, 20, 20, 15, 0, -30,
-    -30, 5, 15, 20, 20, 15, 5, -30,
-    -30, 0, 10, 15, 15, 10, 0, -30,
-    -40, -20, 0, 0, 0, 0, -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50};
-
-    static int[] bishopTable = {
-        -20, -10, -10, -10, -10, -10, -10, -20,
-        -10, 5, 0, 0, 0, 0, 5, -10,
-        -10, 10, 10, 10, 10, 10, 10, -10,
-        -10, 0, 10, 10, 10, 10, 0, -10,
-        -10, 5, 5, 10, 10, 5, 5, -10,
-        -10, 0, 5, 10, 10, 5, 0, -10,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -20, -10, -10, -10, -10, -10, -10, -20};
-
-    static int[] rookTable = {
-        0, 0, 0, 5, 5, 0, 0, 0,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        5, 10, 10, 10, 10, 10, 10, 5,
-        0, 0, 0, 0, 0, 0, 0, 0};
-
-    static int[] queenTable = {
-        -20, -10, -10, -5, -5, -10, -10, -20,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -10, 5, 5, 5, 5, 5, 0, -10,
-        0, 0, 5, 5, 5, 5, 0, -5,
-        -5, 0, 5, 5, 5, 5, 0, -5,
-        -10, 0, 5, 5, 5, 5, 0, -10,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -20, -10, -10, -5, -5, -10, -10, -20};
-
-    static int[] kingTable = {
-        20, 30, 10, 0, 0, 10, 30, 20,
-        20, 20, 0, 0, 0, 0, 20, 20,
-        -10, -20, -20, -20, -20, -20, -20, -10,
-        -20, -30, -30, -40, -40, -30, -30, -20,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30};
-
-    int[][] piecePositionTable = {pawnTable, knightTable, bishopTable, rookTable, queenTable, kingTable};
+    int GetPositionScore(int pieceType, int index) =>
+        (sbyte)((piecePositionValueTable[pieceType * 4 + index / 16] >> (8 * (7 - (index % 8 < 4 ? index % 8 : 7 - index % 8) + index % 16 / 8 * 4))) & 0xFF);
     
     public Move Think(Board board, Timer timer)
     {
@@ -115,8 +55,9 @@ public class MyBot : IChessBot
         //     }
         // }
 
-        Console.WriteLine($"TEST pawn: {GetPositionScore(0, 12)}\n");
-        Console.WriteLine($"TEST knight: {GetPositionScore(1, 0)}\n");
+        Console.WriteLine($"TEST pawn: {GetPositionScore(0, 35)}. (should be: 25)\n");
+        Console.WriteLine($"TEST king: {GetPositionScore(5, 63)}. (should be: -30)\n");
+        Console.WriteLine($"TEST queen: {GetPositionScore(4, 58)}. (should be: -10)\n");
 
         Move[] moves = board.GetLegalMoves();
         moves = OrderMoveByMVVLVA(moves);
@@ -223,7 +164,7 @@ public class MyBot : IChessBot
             {
                 // need to reverse index for black player
                 int index = sign == 1 ? piece.Square.Index : 63 - piece.Square.Index;
-                score += sign * piecePositionTable[i % 6][index];
+                score += sign * GetPositionScore(i % 6, index);
             }
         }
         // King safety evaluation.
---
+++ IntToHex/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] pawnTable = { 0, 0, 0, 0, 0, 0, 0, 0,
+								5, 10, 10, -20, -20, 10, 10, 5,
+								5, -5, -10, 0, 0, -10, -5, 5,
+								0, 0, 0, 20, 20, 0, 0, 0,
+								5, 5, 10, 25, 25, 10, 5, 5,
+								10, 10, 20, 30, 30, 20, 10, 10,
+								50, 50, 50, 50, 50, 50, 50, 50,
+								0, 0, 0, 0, 0, 0, 0, 0 };
+		int[] knightTable = {
+		-50, -40, -30, -30, -30, -30, -40, -50,
+		-40, -20, 0, 5, 5, 0, -20, -40,
+		-30, 5, 10, 15, 15, 10, 5, -30,
+		-30, 0, 15, 20, 20, 15, 0, -30,
+		-30, 5, 15, 20, 20, 15, 5, -30,
+		-30, 0, 10, 15, 15, 10, 0, -30,
+		-40, -20, 0, 0, 0, 0, -20, -40,
+		-50, -40, -30, -30, -30, -30, -40, -50};
+
+		int[] bishopTable = {
+			-20, -10, -10, -10, -10, -10, -10, -20,
+			-10, 5, 0, 0, 0, 0, 5, -10,
+			-10, 10, 10, 10, 10, 10, 10, -10,
+			-10, 0, 10, 10, 10, 10, 0, -10,
+			-10, 5, 5, 10, 10, 5, 5, -10,
+			-10, 0, 5, 10, 10, 5, 0, -10,
+			-10, 0, 0, 0, 0, 0, 0, -10,
+			-20, -10, -10, -10, -10, -10, -10, -20};
+
+		int[] rookTable = {
+			0, 0, 0, 5, 5, 0, 0, 0,
+			-5, 0, 0, 0, 0, 0, 0, -5,
+			-5, 0, 0, 0, 0, 0, 0, -5,
+			-5, 0, 0, 0, 0, 0, 0, -5,
+			-5, 0, 0, 0, 0, 0, 0, -5,
+			-5, 0, 0, 0, 0, 0, 0, -5,
+			5, 10, 10, 10, 10, 10, 10, 5,
+			0, 0, 0, 0, 0, 0, 0, 0};
+
+		int[] queenTable = {
+			-20, -10, -10, -5, -5, -10, -10, -20,
+			-10, 0, 0, 0, 0, 0, 0, -10,
+			-10, 5, 5, 5, 5, 5, 0, -10,
+			0, 0, 5, 5, 5, 5, 0, -5,
+			-5, 0, 5, 5, 5, 5, 0, -5,
+			-10, 0, 5, 5, 5, 5, 0, -10,
+			-10, 0, 0, 0, 0, 0, 0, -10,
+			-20, -10, -10, -5, -5, -10, -10, -20};
+
+		int[] kingTable = {
+			20, 30, 10, 0, 0, 10, 30, 20,
+			20, 20, 0, 0, 0, 0, 20, 20,
+			-10, -20, -20, -20, -20, -20, -20, -10,
+			-20, -30, -30, -40, -40, -30, -30, -20,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30,
+			-30, -40, -40, -50, -50, -40, -40, -30};
+
+		int[][] piecePositionTable = {pawnTable, knightTable, bishopTable, rookTable, queenTable, kingTable};
+
+	// convert each 8-bit int to 8-bit binary hex string, and then combine every 8 of them to 64-bit binary string in hex format
+		foreach (int[] pieceTable in piecePositionTable)
+        {
+            string[] hexTable = new string[64];
+            for (int i = 0; i < 64; i++)
+            {
+                hexTable[i] = Convert.ToString((byte)pieceTable[i], 16).ToUpper();
+                if (hexTable[i].Length == 1)
+                {
+                    hexTable[i] = "0" + hexTable[i];
+                }
+            }
+            string[] hexStrings = new string[4];
+            for (int i = 0; i < 8; i += 2)
+            {
+                hexStrings[i/2] = string.Join("", hexTable.Skip(i * 8).Take(4));
+				hexStrings[i/2] = hexStrings[i/2] + string.Join("", hexTable.Skip(i * 8 + 8).Take(4));
+            }
+            Console.Write("{");
+            foreach (string str in hexStrings)
+                Console.Write("0x" + str + ", ");
+            Console.Write("}");
+            Console.WriteLine();
+        }
+
+	}
+}
